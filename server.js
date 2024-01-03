ddb075f95c2af83d4c736fc39a4feeeba83f5319
ayushmakafle <amikafle1@gmail.com>
2024-01-03 15:35:18 +0545

    setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const colors = require("colors")
+const dotenv = require("dotenv")
+
+//env config
+dotenv.config()
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.get('/',(req,res) => {
+    res.status(200).send({
+        message: "node server"
+    })
+})
+
+const PORT = process.env.PORT || 8080
+//listen
+app.listen(PORT, () => {
+    console.log(`server running ${process.env.DEV_MODE} on port ${PORT}`.bgCyan.white)
+})
