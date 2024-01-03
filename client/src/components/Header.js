cce8514b88aa62f0f7da88feef87189533979a5f
ayushmakafle <amikafle1@gmail.com>
2024-01-04 00:41:59 +0545

    logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link , useNavigate} from 'react-router-dom';
+import { useSelector,useDispatch } from 'react-redux';
+import { authActions } from '../redux/store';
+import { toast } from 'react-toastify';
+
 
 const Header = () => {
 
+    const navigate = useNavigate()
+
     //global state
     const isLogin = useSelector((state) => state.isLogin)
+    const dispatch = useDispatch()
     console.log(isLogin)
     //states
     const [value,setValue]= useState()
+
+    //logout 
+    const handleLogout = () => {
+        try{
+            dispatch(authActions.logout())
+            toast.success('Logout successful')
+            navigate('/login')
+        }catch(error){
+            console.log(error)
+        }
+    }
     
   return (
     <>
@@ -41,7 +58,8 @@ const Header = () => {
                         to ='/register'>Register</Button>
                 </>
             )}
-            {isLogin && (<Button sx={{ margin: 1, color: 'white' }}>Logout</Button>)}
+            {isLogin && (
+                <Button onClick={handleLogout} sx={{ margin: 1, color: 'white' }}>Logout</Button>)}
             
           </Box>
         </Toolbar>
