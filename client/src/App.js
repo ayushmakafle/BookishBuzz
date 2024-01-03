cdf50bfd48b986df09108331c421f21084bb88b1
ayushmakafle <amikafle1@gmail.com>
2024-01-03 22:11:53 +0545

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import Header from "./components/Header";
+import {Routes,Route} from 'react-router-dom'
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Blogs from "./pages/Blogs";
 
 function App() {
   return (
-    <div>
-     <Header />
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path = '/' element={<Blogs />}/>
+        <Route path = '/blogs' element={<Blogs />}/>
+        <Route path = '/login' element={<Login/>}/>
+        <Route path = '/register' element={<Register/>}/>
+      </Routes>
+
+    </>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Box, AppBar, Toolbar, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, AppBar, Toolbar, Button, Typography,Tabs,Tab } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [value,setValue]= useState()
+    
   return (
     <>
       <AppBar position='sticky' sx={{ background: '#f8408f'  }}>
@@ -9,9 +12,22 @@ const Header = () => {
           <Typography variant='h4' sx={{ flexGrow: 1, color: 'white' }}>
             Bookish Buzz
           </Typography>
-          <Box display='flex' marginLeft='auto'>
-            <Button sx={{ margin: 1, color: 'white' }}>Login</Button>
-            <Button sx={{ margin: 1, color: 'white' }}>Register</Button>
+          <Box display={'flex'} marginLeft='auto' marginRight={'auto'}>
+            <Tabs textColor='inherit' value = {value}  onChange={(e,val) => setValue(val)}  indicatorColor='primary' // Set the default indicator color
+                            sx={{ "& .MuiTabs-indicator": { backgroundColor: '#fcadcf' } }} // Customize the indicator color
+            >
+                
+                <Tab label="Blogs" LinkComponent={Link} to='/blogs'/>
+                <Tab label="My Blogs" LinkComponent={Link} to='/my-blogs'/>
+            </Tabs>
+          </Box>
+          <Box display={'flex'} marginLeft='auto'>
+            <Button sx={{ margin: 1, color: 'white' }}
+                LinkComponent={Link} 
+                to ='/login'> Login</Button>
+            <Button sx={{ margin: 1, color: 'white' }}
+                LinkComponent={Link} 
+                to ='/register'>Register</Button>
             <Button sx={{ margin: 1, color: 'white' }}>Logout</Button>
           </Box>
         </Toolbar>
