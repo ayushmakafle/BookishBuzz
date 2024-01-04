99ec69100f0ebede5fd8db8397875eb841e0cfc8
ayushmakafle <amikafle1@gmail.com>
2024-01-04 18:32:05 +0545

    landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -22,47 +22,71 @@ const LandingPage = () => {
     console.log('Component mounted. isLoggedIn:', isLoggedIn);
   }, []);
 
-  return (
-    <Box
-      display="flex"
-      justifyContent="space-between"
-      alignItems="center"
-      padding="50px"
-      backgroundColor="#f8408f"
-      color="white"
-    >
-      {/* Left side - Welcome message */}
-      <Box flex="1">
-        <Typography variant="h2" marginBottom="20px">
-          Welcome to Bookish Buzz
-        </Typography>
-        <Typography variant="body1">
-          Bookish Buzz allows users to update blogs about various books they've read.
-        </Typography>
-        <Button
-          variant="contained"
-          color="secondary"
-          size="large"
-          sx={{ marginTop: '20px' }}
-          component={Link}
-          to="/login"
-          onClick={handleGetStarted}
-        >
-          Get Started
-        </Button>
-      </Box>
+return (
+  <Box
+    display="flex"
+    justifyContent="space-between"
+    alignItems="center"
+    padding="50px"
+    backgroundColor="#f8408f"
+    color="white"
+  >
+    {/* Left side - Welcome message */}
+    <Box flex="1">
+  <Typography variant="h6" marginBottom="3px" color="white">
+    <span style={{ color: '#fcadcf', fontSize: '1.2em', fontWeight: 'bold', marginBottom: '0', padding: '0' }}>
+      <Typography component="h3" variant="h3" style={{ color: 'white', margin: 0 }}>
+        Discover,
+      </Typography>
+    </span>{' '}
+    <br />
+    <span style={{ color: '#fcadcf', fontSize: '1.2em', fontWeight: 'bold', marginBottom: '0', padding: '0' }}>
+      <Typography component="h3" variant="h3" style={{ color: 'white', margin: 0 }}>
+        Connect,
+      </Typography>
+    </span>{' '}
+    <br />
+    <span style={{ color: '#fcadcf', fontSize: '1.2em', fontWeight: 'bold', marginBottom: '0', padding: '0' }}>
+      <Typography component="h3" variant="h3" style={{ color: 'white', margin: 0 }}>
+        Share
+      </Typography>
+    </span>{' '}
+    <br />
+    your Bookish Tales <br />
+    Join the Buzz at Bookish Buzz!
+  </Typography>
 
-      {/* Right side - Image */}
-      <Box flex="1">
-        {/* Replace the URL with the actual URL of your image */}
-        <img
-          src="https://placekitten.com/400/400" // Example image URL
-          alt="Bookish Buzz Image"
-          style={{ width: '100%', borderRadius: '10px' }}
-        />
-      </Box>
+  <Button
+    variant="contained"
+    color="secondary"
+    size="large"
+    sx={{
+      marginTop: '20px',
+      backgroundColor: 'white',
+      color: '#f8408f',
+      '&:hover': {
+      backgroundColor: '#fce4ec', 
+    },
+    }}
+    onClick={handleGetStarted}
+  >
+    Get Started
+  </Button>
+</Box>
+
+
+    {/* Right side - Image */}
+    <Box flex="1">
+      
+      <img
+        src="/images/cute-girl-reading-book-illustrat.png" 
+        alt="Bookish Buzz Image"
+        style={{ width: '100%', maxWidth: '500px', borderRadius: '10px' }}
+      />
     </Box>
-  );
+  </Box>
+);
+
 };
 
 export default LandingPage;
