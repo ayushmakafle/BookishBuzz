3da77fc474481a4487eae19b65a1a6ed371f5f85
ayushmakafle <amikafle1@gmail.com>
2024-01-04 21:00:46 +0545

    display image in all blogs

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,4 +1,3 @@
-// BlogCard.js
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -12,42 +11,41 @@ import { Box, IconButton } from '@mui/material';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios'
-import {toast} from 'react-toastify'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-const BlogCard = ({ title, description, image, username, time,id, isUser }) => {
-
-  const navigate = useNavigate()
+const BlogCard = ({ title, description, image, username, time, id, isUser }) => {
+  const navigate = useNavigate();
   const formattedDate = moment(time).format('YYYY-MM-DD HH:mm:ss');
 
   const handleEdit = () => {
-    navigate(`/blog-details/${id}`)
-  }
+    navigate(`/blog-details/${id}`);
+  };
 
-  const handleDelete = async() => {
-    try{
-      const {data} = await axios.delete (`/api/v1/blog/delete-blog/${id}`)
-      if(data?.success){
-        toast.success('Blog deleted successfully')
-        navigate('/my-blogs')
+  const handleDelete = async () => {
+    try {
+      const { data } = await axios.delete(`/api/v1/blog/delete-blog/${id}`);
+      if (data?.success) {
+        toast.success('Blog deleted successfully');
+        navigate('/my-blogs');
       }
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   return (
     <Card sx={{ width: '100%', margin: 'auto', mt: 2, padding: 2, boxShadow: '5px 5px 10px #ccc', ':hover:': { boxShadow: '10px 10px 20px #ccc' } }}>
       {isUser && (
         <Box display={'flex'}>
-          <IconButton onClick = {handleEdit} sx={{ marginLeft: 'auto' }}>
-            <EditIcon  sx={{ color: '#f8408f' }} />
+          <IconButton onClick={handleEdit} sx={{ marginLeft: 'auto' }}>
+            <EditIcon sx={{ color: '#f8408f' }} />
           </IconButton>
           <IconButton onClick={handleDelete}>
             <DeleteForeverIcon sx={{ color: '#f8408f' }} />
           </IconButton>
         </Box>
-        )}
+      )}
       <CardHeader
         avatar={<Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">{username}</Avatar>}
         title={username}
@@ -56,7 +54,7 @@ const BlogCard = ({ title, description, image, username, time,id, isUser }) => {
       <CardMedia
         component="img"
         height="194"
-        image={image}
+        src={image.dataUri}  // Use `src` instead of `image`
         alt={title}
       />
       <CardContent>
