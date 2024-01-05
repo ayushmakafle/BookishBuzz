4c212968a5fadb99fbd9de79725443a09adccebe
ayushmakafle <amikafle1@gmail.com>
2024-01-05 19:47:55 +0545

    blog card

--- client/src/components/SingleBlogCard.js
+++ client/src/components/SingleBlogCard.js
@@ -37,7 +37,7 @@ const SingleBlogCard = ({ title, image, username, time, id, isUser }) => {
   };
 
   return (
-    <Card sx={{ width: '70%', margin: 'auto', mt: 2, padding: 2, boxShadow: '5px 5px 10px #ccc', ':hover:': { boxShadow: '10px 10px 20px #ccc' } }}>
+    <Card sx={{ width: '100%', margin: 'auto', mt: 2, padding: 2, boxShadow: '5px 5px 10px #ccc', ':hover:': { boxShadow: '10px 10px 20px #ccc' } }}>
       
       {isUser && (
         <Box display={'flex'}>
